992a797209bdab6dc011a2652a5e1c8b796b35fd
Ananth Vivekanand <ananthvivekanand@gmail.com>
2017-03-18 18:10:36 -0500

    Fixed image sizing, initial loading, and bug in slideshow

--- js/1.js
+++ js/1.js
@@ -1,4 +1,13 @@
-elements = document.getElementsByClassName("img-responsive");
-for (var i = 0; i < elements.length; i++) {
-elements[i].style.height = $('#firstphoto').height() + 'px';
-}
+var constant = $('#firstphoto').height();
+    var timestampArray = document.getElementsByClassName("thumbnail");
+
+    for(var i = (timestampArray.length - 1); i >= 0; i--)
+    {
+        timestampArray[i].style.height = constant + 'px';
+    }
+
+
+
+document.getElementById("slide1").src="images/1.jpg";
+document.getElementById("slide2").src="images/2.jpg";
+document.getElementById("slide3").src="images/3.jpg";
