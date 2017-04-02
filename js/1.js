71f8a519b89468b879b5ac517e27adf0fd427c69
Ananth Vivekanand <ananthvivekanand@gmail.com>
2017-04-01 21:13:23 -0500

    Alpha 1.1 Major Update
    Too long to type... :)

--- js/1.js
+++ js/1.js
@@ -7,7 +7,85 @@ var constant = $('#firstphoto').height();
     }
 
 
-
 document.getElementById("slide1").src="images/1.jpg";
 document.getElementById("slide2").src="images/2.jpg";
 document.getElementById("slide3").src="images/3.jpg";
+
+
+$.fn.makeItRain = function(){
+
+
+
+		$(this).on('click',function(){
+
+
+
+			var maxBills = 50;
+
+
+
+
+
+			for (var i = 0; i < maxBills; i++){
+
+
+
+			var random = $(window).width();
+
+
+
+			var randomPosition = Math.floor(random*Math.random());
+
+
+
+			var randomTime = Math.random() * 20;
+
+			var randomSpeed = (Math.random()*20)+10 ;
+
+
+
+
+
+			var bills = $("<span class='billsBillsBills'>")
+
+				.css({
+
+					left : randomPosition,
+
+					top: '-150px',
+
+					"-webkit-animation-delay" : randomTime + "s",
+
+					"-webkit-animation-duration" : randomSpeed + "s"
+
+				});
+
+
+
+				$(bills).prepend('<img src="images/bill.svg" alt="a dollar bill">');
+
+
+
+
+
+				$('#wrapper').append(bills);
+
+
+
+			}; // end click function
+
+
+
+		}); //end for loop
+
+
+
+	}; //end make it rain fn.
+
+
+
+	// thanks to Anisha Varghese from the Noun Project for the SVG!!
+
+
+
+$('#button').makeItRain();
--- js/4.js
+++ js/4.js
@@ -1,27 +1,101 @@
+﻿var options = {
+  useEasing : true, 
+  useGrouping : true, 
+  separator : ',', 
+  decimal : '.', 
+  prefix : '', 
+  suffix : '' 
+};
+var demo = new CountUp(document.getElementsByClassName("card-title")[0], 0, 100, 0, 2.5, options);
+var demo1 = new CountUp(document.getElementsByClassName("card-title")[1], 0, 9, 0, 2.5, options);
+$(function(){
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop(); // how many pixels you've scrolled
+        var os = $('#home').offset().top; // pixels to the top of div1
+        var ht = $('#home').height(); // height of div1 in pixels
+        // if you've scrolled further than the top of div1 plus it's height
+        // change the color. either by adding a class or setting a css property
+        if(scroll > os + ht){
+            demo.start(); 
+            demo1.start();
+        }
+    });
+});
 
-$(document).ready(function(){
-  // Add scrollspy to <body>
-  $('body').scrollspy({target: ".navbar", offset: ((verge.viewportH() - document.getElementById("totalnav").offsetHeight) / 2) + document.getElementById("totalnav").offsetHeight});   
-
-  // Add smooth scrolling on all links inside the navbar
-  $("#myNavbar a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-      scrollamount = $(hash).offset().top - document.getElementById("totalnav").offsetHeight;
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({ 
-        scrollTop: scrollamount
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
+function changePerson(index, elem) {
+switch (index) {
+    case 0:
+        name = "Saturday";
+        break;
+    case 1:
+        name = "Monday";
+        break;
+    case 2:
+        name = "Tuesday";
+        break;
+    case 3:
+        name = "Wednesday";
+        break;
+    case 4:
+        name = "Thursday";
+        break;
+    case 5:
+        name = "Friday";
+        break;
+    case 6:
+        name = "Saturday";
+}
+document.getElementById("name").innerHTML = name;
+}
+      $('.carousel').carousel({
+            dist:-50,
+            shift:0,
+            padding:200,
+	onCycleTo: function (ele, dragged) {
+		changePerson($(ele).index(), ele);
+	}
 
       });
-    }  // End if
-  });
-});
+setTimeout(function(){ $('#clockdiv').fadeIn(); }, 30000);
+setTimeout(function(){ $('#clockdiv').fadeOut(); }, 40000);
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(2017, 3, 12, 15, 45, 0);
+initializeClock('clockdiv', deadline);
